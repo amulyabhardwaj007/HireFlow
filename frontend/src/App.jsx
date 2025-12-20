b3229e95f5c8f7566c7ae2a443ce0a4fd76888cd
Amulya Bhardwaj <amulya.2022UG2076@iiitranchi.ac.in>
2025-12-20 23:42:39 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { useUser } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/HomePage.jsx";
 
 import { Toaster } from "react-hot-toast";
-import DashboardPage from "./pages/DashboardPage";
-import ProblemPage from "./pages/ProblemPage";
-import ProblemsPage from "./pages/ProblemsPage";
-import SessionPage from "./pages/SessionPage";
+import DashboardPage from "./pages/DashboardPage.jsx";
+import ProblemPage from "./pages/ProblemPage.jsx";
+import ProblemsPage from "./pages/ProblemsPage.jsx";
+import SessionPage from "./pages/SessionPage.jsx";
 
 function App() {
   const { isSignedIn, isLoaded } = useUser();
